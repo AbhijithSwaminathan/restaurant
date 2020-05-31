9b2099f0629e9c702903a1742d0d182f73c337ee
Abhijith S <abhiachu2@gmail.com>
2020-05-31 11:45:59 +0530

    finished

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,5 +1,8 @@
 class Worker < ApplicationRecord
     validates :name, presence: true
+    validates :gender, presence: true
+    validates :role, presence: true
+    validates :address, presence: true
     validates :age, numericality: { only_integer: true, :greater_than_or_equal_to => 0.0  } 
     validates :wage, numericality:{:greater_than_or_equal_to => 0.0}
 end
